c2e7f252d90acdd773183405059941cc06b53aa5
Steven Dunn <Dunn.Steven1@gmail.com>
2021-06-28 22:15:44 -0700

    The buttons are now rendered using the utils.range function provided by the author.

--- src/StarMatch/StarMatch.js
+++ src/StarMatch/StarMatch.js
@@ -35,15 +35,10 @@ const StarMatch = () => {
           <StarList starCount={starCount}/>
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+          {/* Identical to the the StarList function, but uses his fancy 'range' function in place instead*/}
+          {utils.range(1, 9).map(number =>
+            <button key="number" className="number">{number}</button>
+          )}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
