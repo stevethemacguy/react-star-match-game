f3ee9082a9a750c60ee2969a3aaac6059c2c899d
Steven Dunn <Dunn.Steven1@gmail.com>
2021-06-28 22:04:09 -0700

    Added a StarList component that loops through a basic number array to create (and return) X number of <Star> elements. The 'React way' is to use a map instead of a for loop, but the end result is identical either way.

--- src/StarMatch/StarMatch.js
+++ src/StarMatch/StarMatch.js
@@ -11,14 +11,17 @@ const Star = () => {
 };
 
 const StarList = (props) => {
-  const elements = []
-  for (let i = 0; i < props.starCount; i++) {
-    elements.push(<Star/>);
-  }
-  return (
-    elements
-  );
+  // If props.starCount is 10, creates an array of numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  // and then uses that array to create (and return) an array of 10 React elements.
+  // This is basically identical to a basic for loop, just done the 'React' Way
+  const countArray = Array.from(Array(props.starCount).keys()) // => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  //const countArray = utils.range(1, props.starCount); // Alternate version that uses his fancy method
+
+  return countArray.map(starId => {
+    return <Star key={starId}/>
+  });
 };
+
 
 const StarMatch = () => {
   const starCount = 9;
