16a8c53a1e3c8ca187d56cf5e87fd9edac7173f8
Steven Dunn <Dunn.Steven1@gmail.com>
2021-07-22 21:12:06 -0700

    Used the author's original 'range' function to fix a bug that occasionally caused the game to enter an un-winnable state where none of the remaining available numbers add up to the total starCount.

--- src/components/StarList.js
+++ src/components/StarList.js
@@ -1,16 +1,29 @@
 import Star from './Star';
+import utils from '../math.utils';
 
 // A list of Star components.
-const StarList = (props) => {
-  // If props.starCount is 10, creates an array of numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  // and then uses that array to create (and return) an array of 10 React elements.
-  // This is basically identical to a basic for loop, just done the 'React' Way
-  const countArray = Array.from(Array(props.starCount).keys()) // => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  //const countArray = utils.range(1, props.starCount); // Alternate version that uses his fancy method
+const StarList = props => (
+  <>
+    {/*Creates an array of numbers, from 1 to starCount, and then uses that array to create the stars.*/}
+    {utils.range(1, props.starCount).map(starId => (
+      <Star key={starId}/>
+    ))}
+  </>
+);
 
-  return countArray.map(starId => {
-    return <Star key={starId}/>
-  });
-};
+// Alternative way to create an array of components without using the author's util function.
+// WARNING: This mostly works, but it creates a bug that occasionally causes the game to enter an un-winnable
+// state where none of the remaining available numbers add up to the total starCount.
+//
+// const StarList = (props) => {
+//   // If props.starCount is 9, creates an array of numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+//   // and then uses that array to create (and return) an array of 9 React elements.
+//   // This is basically identical to a basic for loop, just done the 'React' Way
+//   const countArray = Array.from(Array(props.starCount).keys()) // => [0, 1, 2, 3, 4, 5, 6, 7, 8]
+//
+//   return countArray.map(starId => {
+//     return <Star key={starId}/>
+//   });
+// };
 
 export default StarList;
